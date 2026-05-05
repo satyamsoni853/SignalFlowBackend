b0eb1e94b5546b7037599553bfa4ff0b9ac5d7f7
SatyamSoni <satyamsoni853@gmail.com>
2026-05-05 20:24:04 +0530

    Fix CORS by allowing all origins for demo

--- src/index.ts
+++ src/index.ts
@@ -17,7 +17,7 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 
 app.use(helmet());
-app.use(cors({ origin: process.env.CLIENT_URL || 'http://localhost:3000' }));
+app.use(cors({ origin: '*' })); // Allow all origins for the demo
 app.use(morgan('dev'));
 app.use(express.json());
 
